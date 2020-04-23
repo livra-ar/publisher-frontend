d2172b92be9dd2f445f2643c9d92e0b50d8e5c96
thamidurm <thamidurm@gmail.com>
2020-04-23 10:22:36 +0530

    replace hardcoded url with url from config service

--- src/app/services/file-upload.service.ts
+++ src/app/services/file-upload.service.ts
@@ -14,11 +14,11 @@ export class FileUploadService {
     private config: ConfigService
   ) {
 
-      this.SERVER_URL = this.config.serverUrl + '/upload';
+      this.SERVER_URL = this.config.serverUrl;
     }
 
     public uploadFiles(formData: FormData, type: 'images' | 'files'){
-      let url = this.SERVER_URL;
+      let url = this.SERVER_URL  + '/upload';
       let fileName = 'file';
       if (type === 'files'){
         url += `/raw/${fileName}`;
@@ -32,9 +32,13 @@ export class FileUploadService {
   }
 
   public deleteFiles(urls){
-    let ids = urls.map(url => url.match(`https?://res.cloudinary.com/db2rl2mxy/[a-z0-9/]+/(.*)\....`)[1]);
+    let ids = urls.map(url => (
+      url.match(`https?://res.cloudinary.com/db2rl2mxy/[a-z0-9/]+/(.*)\....`)[1]
+      .replace('.zip', '')
+    ));
+
     for (let id of ids){
-      this.http$.delete<any>('http://localhost:8000/files/delete/'+id+'/',).subscribe();
+      this.http$.delete<any>(`${this.SERVER_URL}/files/delete/id/`,).subscribe();
     }
   }
 
