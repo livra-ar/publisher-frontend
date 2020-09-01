4327c00e60a1f2a764b2866ff50edef76aee26a9
thamidurm <thamidurm@gmail.com>
2020-09-01 21:31:07 +0530

    mobile user guide addition

---
+++ src/app/mobile-user-guide/mobile-user-guide.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mobile-user-guide',
+  templateUrl: './mobile-user-guide.component.html',
+  styleUrls: ['./mobile-user-guide.component.scss']
+})
+export class MobileUserGuideComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
