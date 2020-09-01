df30a17b74a3e64aba204652cf76f77120afef0a
thamidurm <thamidurm@gmail.com>
2020-09-01 21:32:31 +0530

    improve dashboard

--- src/app/dashboard/dashboard.component.spec.ts
+++ src/app/dashboard/dashboard.component.spec.ts
@@ -1,25 +1,129 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import {of} from 'rxjs';
+import { Component, OnInit } from '@angular/core';
 import { DashboardComponent } from './dashboard.component';
+import { BooksService } from '@app/services/books.service';
+import { ContentService } from '@app/services/content.service';
+import { AuthService } from '@app/auth/auth.service';
+import { ComponentFixtureAutoDetect } from '@angular/core/testing';
+const books =   [{
+        id: 'abcd1234',
+        title: 'Title',
+        authors: ['Author K.'],
+        active: true,
+        isbns: ['1234567890'],
+        covers: ['https://localhost/cover.png'],
+        publisher: {
+          id: '123456789'
+        }
+      }];
+const content = [
+      {
+        id: 'efgh1234',
+        title: 'Content',
+        description:'This is a mock content',
+        size: 4000,
+        creator : {
+          id: '123456789',
+          name: 'UserName'
+        },
+        active: true,
+        images: ['https://localhost/preview.png'],
+        files: ['https://localhost/file.sfb'],
+        book: books[0]
+      }
+    ];
+
+ let expectedBooks = [
+   {
+        link: '/books/'+ books[0].id,
+        title: books[0].title,
+        image: books[0].covers[0],
+        type: 'book',
+        active: books[0].active,
+        owner: books[0].publisher.id
+   }
+ ];
+
+  let expectedContent = [
+   {
+        link: '/content/'+ content[0].id,
+        title: content[0].title,
+        image: content[0].images[0],
+        type: 'content',
+        active: content[0].active,
+        owner: content[0].creator.id
+   }
+ ];
+class MockBookService{
+  getUsersBooks(){
+    return of(books);
+  }
+}
+
+class MockContentService{
+  getUsersContent(){
+    return of(content);
+  }
+}
+
+
+class MockAuthService{
+  getCurrentUser(){
+    return of({});
+  }
+}
+
+
+
+@Component({selector: 'app-card-list', template: ''})
+class CardListComponent {}
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
-
+  let booksService;
+  let contentService;
+  let authService;
+  let booksCardList;
+  let contentCardList;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DashboardComponent ]
+      declarations: [ DashboardComponent, CardListComponent ],
+      providers : [
+        {provide: BooksService, useClass: MockBookService},
+        {provide: ContentService, useClass: MockContentService},
+        {provide: AuthService, useClass: MockAuthService},
+        { provide: ComponentFixtureAutoDetect, useValue: true }
+      ]
     })
     .compileComponents();
+    booksService = TestBed.inject(BooksService);
+    contentService = TestBed.inject(ContentService);
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(DashboardComponent);
     component = fixture.componentInstance;
+    booksCardList = fixture.nativeElement.querySelector('#books-card-list');
+    contentCardList = fixture.nativeElement.querySelector('#content-card-list');
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should populate content$ and books$ after Angular calls ngOnInit', () => {
+    component.ngOnInit();
+
+    component.books$.subscribe(_books => expect(_books).toEqual(expectedBooks));
+    component.content$.subscribe(_content => expect(_content).toEqual(expectedContent));
+    fixture.detectChanges();
+    /*
+      Need to test if the props are set
+    */
+  });
+
+  
 });
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,7 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { BooksService } from '@app/services/books.service';
 import { ContentService } from '@app/services/content.service';
+import { AuthService } from '@app/auth/auth.service';
 import { map } from 'rxjs/operators';
+import { BehaviorSubject } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
+import { FullscreenSearchComponent } from '@app/fullscreen-search/fullscreen-search.component';
+import { MatTabChangeEvent } from '@angular/material/tabs/tab-group';
+import * as JsSearch from 'js-search';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,35 +16,197 @@ import { map } from 'rxjs/operators';
 })
 export class DashboardComponent implements OnInit {
 
+  bookResults = 0;
+  contentResults = 0;
+  bookSearch: any;
+  contentSearch: any;
+  bookIndex:number = 0;
+  contentIndex:number = 0;
+  booksLoading: boolean = true;
+  contentLoading:boolean = true;
   constructor(
     private booksService: BooksService,
-    private contentSerivce: ContentService
+    private contentSerivce: ContentService,
+    private authService: AuthService,
+    private dialog: MatDialog
   ) { }
-  books$;
-  content$;
+  books$ = new BehaviorSubject([]);
+  content$ = new BehaviorSubject([]);
+  content_count = 0;
+  book_count = 0;
+
+  user;
+  query = {
+    content: '',
+    books : ''
+  };
+  currentTab : ("books" | "content") = "content";
+  // navLinks = [
+  //       {
+  //           label: 'Content',
+  //           link: './content',
+  //           index: 0
+  //       }, {
+  //           label: 'Books',
+  //           link: './books',
+  //           index: 1
+  //       },
+  //   ];
   ngOnInit(): void {
-    this.books$ = this.booksService.getUsersBooks().pipe(
-      map(books => {
-        return books.map(book => ({
-          link: '/books/'+book.id,
-          title: book.title,
-          image: (book.covers) ? book.covers[0]: null,
-          type: 'book',
-          active: book.active
-        }));
-      })
-      );
-    this.content$ = this.contentSerivce.getUsersContent().pipe(
+    this.user = this.authService.getCurrentUser();
+    this.bookSearch = new JsSearch.Search('id');
+    this.bookSearch.addIndex('title');
+    this.contentSearch = new JsSearch.Search('id');
+    this.contentSearch.addIndex('title');
+    
+    this.fetchBooks(0, 8);
+    this.fetchContent(0, 8);
+  }
+
+  fetchContent(index, size, query=null, cache=true){
+    this.contentLoading = true;
+    this.contentSerivce.getUsersContent(index, size,cache).pipe(
       map(contents => {
-        return contents.map(content => ({
+         this.content_count  = contents.length;
+
+          if(size === -1){
+            size = contents.length;
+          } else {
+            size = Math.min(contents.length, size);
+          }
+
+          index = Math.max(0, index);
+          this.contentSearch.addDocuments(contents);
+
+          if(query)
+            contents =  this.contentSearch.search(query);
+
+          this.contentResults = contents.length;
+
+          this.content$.next(contents.map(content => ({
           link: '/content/'+content.id,
           title: content.title,
           image: (content.images) ? content.images[0]: null,
           active: content.active,
-          type:'content'
-        }));
+          type:'content',
+          owner: content.creator.id
+        })).slice(index*size, index*size + size ));
+
+        this.contentLoading = false;
+
       })
-      );
+      ).subscribe();
+  }
+  fetchBooks(index, size, query = null, cache=true){
+    this.booksLoading = true;
+    this.booksService.getUsersBooks(index, size, cache).pipe(
+      map(books => {
+
+        this.book_count  = books.length;
+        
+        if(size === -1){
+          size = books.length;
+        } else {
+          size = Math.min(books.length, size);
+        }
+
+        index = Math.max(0, index);
+
+        this.bookSearch.addDocuments(books);
+
+        if(query)
+          books =  this.bookSearch.search(query);
+
+        this.bookResults = books.length;  
+        this.books$.next(
+          books.map(book => 
+            ({
+              link: '/books/'+book.id,
+              title: book.title,
+              image: (book.covers) ? book.covers[0]: null,
+              type: 'book',
+              active: book.active,
+              owner: book.publisher.id
+            })
+          ).slice(index*size, index*size + size)
+        );
+
+        this.booksLoading =false;
+      })
+      ).subscribe();
+  }
+
+  loadItems(pageIndex, pageSize, query, cache=true){
+    switch (this.currentTab){
+      case "books":
+        this.fetchBooks(pageIndex, pageSize, query, cache);
+        break;
+      case "content":
+        this.fetchContent(pageIndex, pageSize, query, cache);
+        break;
+    }
+  }
+
+  reload(){
+    switch (this.currentTab){
+      case "books":
+        this.loadItems(this.bookIndex, this.bookPageSize, this.query.books, false);
+        break;
+      case "content":
+        this.loadItems(this.contentIndex, this.contentPageSize, this.query.content, false);
+        break;
+    }
+  }
+
+  onPageEvent(event){
+    let query;
+    switch (this.currentTab){
+      case "books":
+        this.bookPageSize = event.pageSize;
+        this.bookIndex = event.pageIndex;
+        query = this.query.books;
+        break;
+      case "content":
+        this.contentPageSize = event.pageSize;
+        this.contentIndex = event.pageIndex;
+        query = this.query.content;
+        break;
+    }
+
+    this.loadItems(event.pageIndex, event.pageSize, query);
+  }
+
+  
+  bookPageSize = 8;
+  contentPageSize = 8;
+
+  tabChanged(tabChangeEvent: MatTabChangeEvent){
+    this.currentTab = (tabChangeEvent.index === 0) ? "content" : "books";
+  }
+
+  queryChange(query){
+
+    let pageSize;
+    if (this.currentTab === "books") {
+      pageSize = this.bookPageSize;
+      this.query.books = query;
+    }else if(this.currentTab === "content"){
+      pageSize = this.contentPageSize;
+      this.query.content = query;
+    }
+    this.loadItems(0, pageSize, query);
+  }
+
+  showSearch(){
+    const dialogRef = this.dialog.open(FullscreenSearchComponent, {
+      height: '115px',
+      width: '320px',
+      disableClose:false,
+      data: {
+        queryChange: this.queryChange.bind(this),
+        query: (this.currentTab === 'books') ? this.query.books : this.query.content
+      }
+    });
   }
 
 }
