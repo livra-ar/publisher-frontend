5dadfe384599024e95a49e7cf5234937f6d0f761
thamidurm <thamidurm@gmail.com>
2020-09-01 21:31:19 +0530

    add pub user guide

---
+++ src/app/publisher-user-guide/publisher-user-guide.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-publisher-user-guide',
+  templateUrl: './publisher-user-guide.component.html',
+  styleUrls: ['./publisher-user-guide.component.scss']
+})
+export class PublisherUserGuideComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
