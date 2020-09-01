25d3f2aea1002c6f93798dc8560f676bc4e3b77a
thamidurm <thamidurm@gmail.com>
2020-09-01 21:34:32 +0530

    shared tests

--- src/app/shared/dialog/dialog.component.spec.ts
+++ src/app/shared/dialog/dialog.component.spec.ts
@@ -1,14 +1,29 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 import { DialogComponent } from './dialog.component';
 
 describe('DialogComponent', () => {
   let component: DialogComponent;
   let fixture: ComponentFixture<DialogComponent>;
-
+  let matDialogRefSpy =  {};//jasmine.createSpyObj('MatDialogRef', []);
+  let data = {
+    title: 'title',
+    type: 'type',
+    message: 'message'
+  };
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DialogComponent ]
+      declarations: [ DialogComponent ],
+      providers : [
+        {
+          provide: MatDialogRef,
+          useValue: matDialogRefSpy
+        },
+        {
+          provide: MAT_DIALOG_DATA,
+          useValue: data
+        }
+      ]
     })
     .compileComponents();
   }));
